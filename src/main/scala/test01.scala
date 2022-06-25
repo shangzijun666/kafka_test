31baa3e314f1517b1f95a9fe72e0616111e0b1c1
szj <15847255484@163.com>
2022-06-26 03:59:38 +0800

    分支 更新 git

--- src/main/scala/test01.scala
+++ src/main/scala/test01.scala
@@ -6,7 +6,9 @@ import org.apache.spark._
 object test01 {
   def main(args: Array[String]): Unit = {
 
-
+    println("hello world  scala ")
+    println("我有来了 提交测试")
+    println("这里是master")
 
   }
 }
