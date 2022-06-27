5eb8ae2401cc7d86ca2d099902226970a3244638
shangzijun666 <15847255484@163.com>
2022-06-27 15:06:16 +0800

    producer 测试

---
+++ src/main/java/Producer_test.java
@@ -0,0 +1,7 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+public class Producer_test {
+    public static void main(String[] args) {
+
+    }
+}
