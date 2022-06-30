edee84bf8d65ff7c47468872231e33f2d8fcc4b6
szj <15847255484@163.com>
2022-07-01 00:02:27 +0800

    测试 kafka 数据源 sparkstreaming消费

---
+++ src/main/scala/streaming/SparkStreaming_1.scala
@@ -0,0 +1,34 @@
+package streaming
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkStreaming_1 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("streaming").setMaster("local[*]")
+    val context = new SparkContext(conf)
+    val ssc = new StreamingContext(context, Seconds(3))
+
+    val prop = Map[String,String](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG->"123.57.236.115:9092",
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG->"org.apache.kafka.common.serialization.StringDeserializer",
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG->"org.apache.kafka.common.serialization.StringDeserializer",
+      ConsumerConfig.GROUP_ID_CONFIG->"streaming"
+    )
+
+    val value: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream(
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](Set("first"), prop)
+    )
+
+    value.map(_.value()).print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
